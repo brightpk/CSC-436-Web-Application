2bbac7a2ed5ea17c6a145cca05574951ab665c3a
Bright Phakamad <bright.sp@hotmail.com>
2019-10-27 09:20:25 -0500

    added components: home, dashboard, new-todo-item, todo-item, todo-list, page-not-found. Added routes for dashboard, home and page-not-found

--- HW4/todo-app/src/app/app.module.ts
+++ HW4/todo-app/src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { NewTodoItemComponent } from './new-todo-item/new-todo-item.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      DashboardComponent,
+      TodoItemComponent,
+      TodoListComponent,
+      NewTodoItemComponent,
+      PageNotFoundComponent,
+      HomeComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
